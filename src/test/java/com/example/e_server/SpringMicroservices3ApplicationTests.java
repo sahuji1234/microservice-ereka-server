f8eb344e43bd7eb330c3fb2c9231c33a1c56f90d
suraj sahu <int-SurajS@Netlink.com>
2022-07-28 18:35:08 +0530

    ereka server

---
+++ src/test/java/com/example/e_server/SpringMicroservices3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservices3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
